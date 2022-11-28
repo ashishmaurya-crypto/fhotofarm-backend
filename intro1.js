038b75d31504da063f8404b1508a0aa4c540fd15
Ashish maurya <62279600+ashishmaurya-crypto@users.noreply.github.com>
2022-11-28 12:59:21 +0530

    push code

---
+++ intro1.js
@@ -0,0 +1,13 @@
+console.log('welocome file intro1')
+console.log(__dirname)
+// setInterval(()=>{
+//     console.log('welocome here')
+// }, 1000)
+
+// modules
+
+const sayHi = (name) =>{
+    console.log(`my name is ${name}`);
+}
+
+module.exports = sayHi;
